0d2f10de43aa15e4c9dbe50cb0ec5902e10d373d
Jonas Kruckenberg <jonas@kruckenbg.de>
2020-04-17 00:34:01 +0200

    chore: :sparkles: mark as side effect free

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
+import { uglify } from "rollup-plugin-uglify";
 import pkg from './package.json'
+
 export default {
   input: './index.ts',
   output: [
@@ -12,7 +14,7 @@ export default {
       file: pkg.module,
       format: 'es',
       sourcemap: true
-    },
+    }
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),

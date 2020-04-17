b7c4390d9038afc29f9dc73e7f004fbd5fdd1606
Jonas Kruckenberg <jonas@kruckenbg.de>
2020-04-17 12:52:56 +0200

    test: :white_check_mark: Add tests for waterfall hooks
    Add tests to the two waterfall hooks, that test the undefined is placeholder behaviour

--- test/AyncSeriesWaterfallHook.test.ts
+++ test/AyncSeriesWaterfallHook.test.ts
@@ -30,13 +30,19 @@ describe('AsyncSeriesWaterfallHook', () => {
       hook.tap((val1, val2) => [val1 + 'C1',val2 + 'C2'])
       expect(await hook.promise('I1','I2')).to.deep.equal(['I1A1B1C1','I2A2B2C2'])
     })
-
     it('works with 3 parameters', async () => {
       const hook = new AsyncSeriesWaterfallHook<[string,string,string]>()
       hook.tap((val1, val2, val3) => [val1 + 'A1',val2 + 'A2',val3 + 'A3'])
       hook.tap((val1, val2, val3) => [val1 + 'B1',val2 + 'B2',val3 + 'B3'])
       hook.tap((val1, val2, val3) => [val1 + 'C1',val2 + 'C2',val3 + 'C3'])
       expect(await hook.promise('I1','I2','I3')).to.deep.equal(['I1A1B1C1','I2A2B2C2','I3A3B3C3'])
+    })
+    it('works with undefined as placeholder', async () => {
+      const hook = new AsyncSeriesWaterfallHook<[string,string]>()
+      hook.tap((val1, val2) => [val1 + 'A1',val2 + 'A2'])
+      hook.tap((val1, val2) => [undefined,val2 + 'B2'])
+      hook.tap((val1, val2) => [val1 + 'C1',undefined])
+      expect(await hook.promise('I1','I2')).to.deep.equal(['I1A1C1','I2A2B2'])
     })
     it('rejects when tap throws', () => {
       hook.tap(() => { throw new Error('dummy') })

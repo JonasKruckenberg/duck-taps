bf28a827f4676549449aa8e2092c5e546ef76a37
Jonas Kruckenberg <jonas@kruckenbg.de>
2020-04-17 23:11:22 +0200

    feat: :sparkles: Add Bounce Hooks

--- lib/SyncBounceHook.ts
+++ lib/SyncBounceHook.ts
@@ -6,63 +6,25 @@ import { merge } from './util'
 * This is useful for for example a chain a taps that produce a response to a request for example.
 * This Hook does not allow to bail, use the `AsyncSeriesBounceBailHook` for that.
 */
-export default class SyncBounceHook<T extends any[]> extends Hook<T> {
+export default class SyncBounceHook<T extends any[]> extends Hook<T,'execute' | 'post'> {
+  phases = ['execute','post']
 
-  name:string
-
-  constructor() {
-    super()
-    throw 'SyncBounceHook is currently not implemented'
+  call( ...args: T ) {
+    let i
+    const ctx:any = {}
+    for ( i = this.taps.length - 1; i >= 0; i--) {
+      if ( !this.taps[i].phases.execute ) continue
+      const res = this.taps[i].phases.execute(...args, ctx )
+      if ( Array.isArray(res) ) args = merge(args, res) as T
+      else if ( res !== undefined ) args[0] = res
+    }
+    i += 2
+    for ( i; i < this.taps.length; i++ ) {
+      if ( !this.taps[i].phases.post ) continue
+      const res = this.taps[i].phases.post(...args, ctx )
+      if ( Array.isArray(res) ) args = merge(args, res) as T
+      else if ( res !== undefined ) args[0] = res
+    }
+    return args
   }
-  // /**
-  // * Invokes the hook with the given arguments and retuns the result of all the taps as an array. The returned Array has the same length as the number of arguments you pass in.
-  // * When an error occured during the *out* phase it will throw and error.
-  // */
-  // call( ...args:any[] ) {
-  //   let lvl = this.taps.length - 1
-  //
-  //   const ctx:any = { phase: 'in' }
-  //
-  //   for ( let i = lvl; i >= 0; i--) {
-  //     if ( i === 0 ) ctx.phase = 'bounce'
-  //     if ( this.taps[i].in ) {
-  //       const res = this.taps[i].in(...args,ctx)
-  //       if (res) merge(this.args,args,res)
-  //     }
-  //     lvl = i
-  //   }
-  //   ctx.phase = 'out'
-  //   for ( let i = lvl + 1; i < this.taps.length; i++ ) {
-  //     if ( !this.taps[i].out ) continue
-  //     const res = this.taps[i].out(...args,ctx)
-  //     if (res) merge(this.args,args,res)
-  //   }
-  //   return args
-  // }
-  //
-  // /**
-  // * Invokes the Hook with the given arguments and returns a promise that resolves when all taps are executed.
-  // * When an error occures during the *out* phase the promise rejects.
-  // */
-  // async promise(...args:any[] ) {
-  //   let lvl = this.taps.length - 1
-  //
-  //   const ctx:any = { phase: 'in' }
-  //
-  //   for ( let i = lvl; i >= 0; i--) {
-  //     if ( i === 0 ) ctx.phase = 'bounce'
-  //     if ( this.taps[i].in ) {
-  //       const res = await this.taps[i].in(...args,ctx)
-  //       if (res) merge(this.args,args,res)
-  //     }
-  //     lvl = i
-  //   }
-  //   ctx.phase = 'out'
-  //   for ( let i = lvl + 1; i < this.taps.length; i++ ) {
-  //     if ( !this.taps[i].out ) continue
-  //     const res = await this.taps[i].out(...args,ctx)
-  //     if (res) merge(this.args,args,res)
-  //   }
-  //   return args
-  // }
 }
